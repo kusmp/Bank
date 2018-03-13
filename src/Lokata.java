ad73403e5624f43d26c3f440faa2ad48367cfaf2
Damian Szalbierz <szalbierz.d.k@gmail.com>
2018-03-13 15:59:05 +0100

    Create constructor, setters and getters for Lokata.

--- src/Lokata.java
+++ src/Lokata.java
@@ -1,20 +1,28 @@
-public class Lokata implements ProduktBankowy {
-
-    private double odsetki;
-    private int czas; //podawane w miesiacach
-    private double procent;
+public class Lokata extends RachunekBankowy implements ProduktBankowy {
     private String rachunek;
     private double srodki;
+    private int czas; //podawane w miesiacach
+    private double procent;
 
-
-    public Lokata(){
-
+    public  Lokata (String rachunek, double srodki, int czas, double procent) {
+        super(rachunek, srodki);
+        this.czas = czas;
+        this.procent = procent;
     }
 
-    public String wyswietlNumerRachunku(){
-        return rachunek;
+    public int getCzas() {
+        return czas;
     }
 
+    public void setCzas(int czas) {
+        this.czas = czas;
+    }
 
+    public double getProcent() {
+        return procent;
+    }
 
+    public void setProcent(double procent) {
+        this.procent = procent;
+    }
 }
