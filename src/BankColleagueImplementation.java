22669563f2761cb31becf1f070557c2c95f530de
kusmp <patryk.kusmierkiewicz@gmail.com>
2018-05-18 13:09:50 +0200

    decorator test

--- src/BankColleagueImplementation.java
+++ src/BankColleagueImplementation.java
@@ -1,20 +1,20 @@
-public class BankColleagueImplementation extends BankColleague {
-
-    public BankColleagueImplementation(BankMediator mediator, String nazwa) {
-        super(mediator, nazwa);
-    }
-
-    @Override
-    public void wyslane(String msg) {
-        System.out.println("Przesylanie pieniedzy");
-        mediator.przelewMiedzybankowy();
-    }
-
-
-    @Override
-    public void odebrane(String msg) {
-
-    }
-}
+//public class BankColleagueImplementation extends BankColleague {
+//
+//    public BankColleagueImplementation(BankMediator mediator, String nazwa) {
+//        super(mediator, nazwa);
+//    }
+//
+//    @Override
+//    public void wyslane(String msg) {
+//        System.out.println("Przesylanie pieniedzy");
+//       // mediator.przelewMiedzybankowy();
+//    }
+//
+//
+//    @Override
+//    public void odebrane(String msg) {
+//
+//    }
+//}
 
 
--- tests/RachunekBankowyTest.java
+++ tests/RachunekBankowyTest.java
@@ -8,25 +8,68 @@ class RachunekBankowyTest {
     void zwiekszSrodki() {
         RachunekBankowy rachunek = new RachunekBankowy();
         rachunek.zwiekszSrodki(500);
-        assertEquals(500,rachunek.pokazSrodki());
+        assertEquals(500,rachunek.getSrodki());
     }
     @Test
     void zmniejszSrodki() {
         RachunekBankowy rachunek = new RachunekBankowy();
         rachunek.setSrodki(600);
         rachunek.zmniejszSrodki(300);
-        assertEquals(300,rachunek.pokazSrodki());
+        assertEquals(300,rachunek.getSrodki());
     }
     @Test
     void zmniejszSrodkiDebet() {
         RachunekBankowy rachunek = new RachunekBankowy();
         rachunek.setSrodki(600);
         rachunek.zmniejszSrodki(1000);
-        assertEquals(600,rachunek.pokazSrodki());
+        assertEquals(600,rachunek.getSrodki());
     }
     @Test
     void pokazSrodki() {
         RachunekBankowy rachunek = new RachunekBankowy();
-        assertEquals(0,rachunek.pokazSrodki());
+        assertEquals(0,rachunek.getSrodki());
+    }
+
+    @Test
+    void testDebetZaDuzo(){
+        RachunekBankowy rachunek = new RachunekBankowy();
+        rachunek.setSrodki(1000);
+        RachunekBankowyDekorator dekorator = new RachunekBankowyDekorator(rachunek, 500);
+        dekorator.zmniejszSrodki(2000);
+        assertEquals(0, dekorator.getDebet());
+        assertEquals(false, dekorator.isDebetExist());
+
+    }
+
+    @Test
+    void testDebetDobrze(){
+        RachunekBankowy rachunek = new RachunekBankowy();
+        rachunek.setSrodki(1000);
+        RachunekBankowyDekorator dekorator = new RachunekBankowyDekorator(rachunek, 500);
+        dekorator.zmniejszSrodki(1400);
+        assertEquals(400, dekorator.getDebet());
+        assertEquals(true, dekorator.isDebetExist());
+
+    }
+
+
+    @Test
+    void testDebet(){
+        RachunekBankowy rachunek = new RachunekBankowy();
+        rachunek.setSrodki(1000);
+        RachunekBankowyDekorator dekorator = new RachunekBankowyDekorator(rachunek, 500);
+        dekorator.zmniejszSrodki(2000);
+        assertEquals(0, dekorator.getDebet());
+        assertEquals(false, dekorator.isDebetExist());
+    }
+
+    @Test
+    void testDebetBezWykorzystania(){
+        RachunekBankowy rachunek = new RachunekBankowy();
+        rachunek.setSrodki(1000);
+        RachunekBankowyDekorator dekorator = new RachunekBankowyDekorator(rachunek, 500);
+        dekorator.zmniejszSrodki(500);
+        assertEquals(0, dekorator.getDebet());
+        assertEquals(false, dekorator.isDebetExist());
     }
 }
